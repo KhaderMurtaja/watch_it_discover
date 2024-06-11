707f1abcf71fd7febf4a44408e454129ed323616
KhaderMurtaja <khadermurtaja1@gmail.com>
2024-06-11 18:51:58 +0300

    Watch the username changes

--- lib/pages/user_name.dart
+++ lib/pages/user_name.dart
@@ -1,33 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:watch_it/watch_it.dart';
+import 'package:watch_it_discover/models/user_model.dart';
 
-/// A page that displays a counter.
-class UserName extends StatefulWidget {
-  /// Constructs the [UserName].
+/// A widget that displays the user name.
+/// It watches the user name from the [UserModel].
+/// It also provides a text field to enter the user name.
+/// The user name is displayed below the text field.
+class UserName extends WatchingWidget {
+  /// Constructor to create a [UserName] widget.
   const UserName({super.key});
 
   @override
-  State<UserName> createState() => _UserNameState();
-}
-
-class _UserNameState extends State<UserName> {
-  @override
   Widget build(BuildContext context) {
+    /// Watch the user name from the [UserModel].
+    final userName = watchPropertyValue((UserModel um) => um.name);
+
+    /// Controller to get the user name from the text field.
+    final nameController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Enter your name:'),
+            const Text('Enter your name:'),
             TextField(
-              decoration: InputDecoration(
+              controller: nameController,
+              decoration: const InputDecoration(
                 hintText: 'Name',
               ),
             ),
-            Text(
+            const Text(
               'The Full Name:',
               style: TextStyle(
                 color: Colors.black45,
