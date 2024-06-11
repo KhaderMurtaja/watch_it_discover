0cb1bd926f178bbbdc4427c56fe8590298fcab63
KhaderMurtaja <khadermurtaja1@gmail.com>
2024-06-11 18:25:41 +0300

    Remove the counter page

--- lib/pages/user_name.dart
+++ lib/pages/user_name.dart
@@ -10,38 +10,32 @@ class UserName extends StatefulWidget {
 }
 
 class _UserNameState extends State<UserName> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(
+      body: const Padding(
+        padding: EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text('Enter your name:'),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Name',
+              ),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'The Full Name:',
+              style: TextStyle(
+                color: Colors.black45,
+                fontSize: 20,
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
